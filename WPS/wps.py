ef2e0677ca06b159900b863b3f4d9539e9c18ff7
lysovosyl <133856851+lysovosyl@users.noreply.github.com>
2025-08-07 15:37:06 +0800

    Add files via upload

--- WPS/wps.py
+++ WPS/wps.py
@@ -1,6 +1,8 @@
 
 import pysam
 import numpy as np
+import argparse
+import csv
 
 def isSoftClipped(cigar):
   for (op,count) in cigar:
@@ -11,7 +13,7 @@ def aln_length(cigarlist):
   for operation,length in cigarlist:
     if operation == 0 or operation == 2 or operation == 3 or operation >= 6: tlength += length
   return tlength
-def wps_signal(sf:pysam.AlignmentFile,windows,chrid,start,end):
+def wps_signal(sf,windows,chrid,start,end):
     # sf = pysam.AlignmentFile(bam_file, "rb")
     '''wps'''
     length = end-start
@@ -87,3 +89,46 @@ def wps_signal(sf:pysam.AlignmentFile,windows,chrid,start,end):
     for i in range(start,end):
         wps_arr_x.append(i)
     return wps_arr,wps_arr_x
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--bed_file', type=str, required=True,
+                        help='Path to the BED file defining target gene regions (e.g., upstream promoters)')
+    parser.add_argument('--bam_file', type=str, required=True,
+                        help='Path to the cfDNA aligned BAM file')
+    parser.add_argument('--o', type=str, required=True,
+                        help='Output path for the resulting (TSV format)')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    bed_file = args.bed_file
+    bam_file = args.bam_file
+    tsv_file = args.o
+    windows = 120
+    sf = pysam.AlignmentFile(bam_file)
+    f = open(bed_file)
+
+    lines = f.readlines()
+    f.close()
+    tsv_data = []
+    for line in lines:
+        tsv_data.append(line[:-1].split('\t'))
+
+    wps_data = {}
+    for chrid, start, end, gene, _ in tsv_data[:]:
+        wps_data[gene],_ = wps_signal(sf,windows,chrid,int(start),int(end))
+        print(wps_data[gene])
+
+    # 找到最长的 WPS 数组长度（用于填充）
+    max_len = max(len(values) for values in wps_data.values())
+    # 保存为 CSV 文件
+    with open(tsv_file, mode='w', newline='') as file:
+        writer = csv.writer(file,delimiter='\t')
+        # 写表头：第一个是 gene 名，后面是每个位置的索引
+        header = ['gene'] + [f'{i}' for i in range(max_len)]
+        writer.writerow(header)
+        # 写入每一行基因和对应的 WPS 值（不足部分填充为空）
+        for gene, values in wps_data.items():
+            row = [gene] + list(values) + [''] * (max_len - len(values))
+            writer.writerow(row)
